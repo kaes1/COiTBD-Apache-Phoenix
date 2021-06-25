349fe9d8b66b26340a8e1ee259db960d9db94084
Krzysztof Sobocik <krzysob384@student.polsl.pl>
2021-06-25 02:42:48 +0200

    Unit Tests - LinguisticValueParser

--- src/main/java/pl/polsl/linguisticVariable/parser/LinguisticValueParser.java
+++ src/main/java/pl/polsl/linguisticVariable/parser/LinguisticValueParser.java
@@ -8,15 +8,10 @@ import pl.polsl.membershipFunction.TriangularMembershipFunction;
 import java.util.Arrays;
 import java.util.List;
 
-
-/**
- * Example:
- * "Height:Low,T,0,0,160,170|Medium,T,160,170,180,190|High,T,180,190,1000,1000"
- */
 public class LinguisticValueParser {
 
-    private final static List<String> TRAPEZOID_MEMBERSHIP_TYPE = Arrays.asList("1", "T", "TRAP", "TRAPEZOID");
-    private final static List<String> TRIANGULAR_MEMBERSHIP_TYPE = Arrays.asList("2", "TRI", "TRIA", "TRIANGULAR");
+    private final static List<String> TRAPEZOID_MEMBERSHIP_TYPE = Arrays.asList("1", "T", "TRAP", "TRAPEZOID", "TRAPEZOIDAL");
+    private final static List<String> TRIANGULAR_MEMBERSHIP_TYPE = Arrays.asList("2", "TRI", "TRIA", "TRIANGLE", "TRIANGULAR");
     private final static List<String> GAUSSIAN_MEMBERSHIP_TYPE = Arrays.asList("3", "G", "GAUSS", "GAUSSIAN");
 
     private final NameParser nameParser;
@@ -37,12 +32,17 @@ public class LinguisticValueParser {
      */
     public LinguisticValue parse(String linguisticValueString) throws LinguisticVariableParserException {
         String[] split = linguisticValueString.split(",");
+
+        if (split.length < 2) {
+            throw new LinguisticVariableParserException("Linguistic value string " + linguisticValueString + " contains too few arguments.");
+        }
+
         String valueName = nameParser.parse(split[0]);
         String membershipType = split[1].toUpperCase();
 
         if (TRAPEZOID_MEMBERSHIP_TYPE.contains(membershipType)) {
             if (split.length < 6) {
-                throw new LinguisticVariableParserException("Not enough arguments for Trapezoidal membership function.");
+                throw new LinguisticVariableParserException("Linguistic value string " + linguisticValueString + "contains too few for Trapezoidal membership function.");
             }
             double a = doubleParser.parse(split[2]);
             double b = doubleParser.parse(split[3]);
@@ -51,7 +51,7 @@ public class LinguisticValueParser {
             return new LinguisticValue(valueName, new TrapezoidalMembershipFunction(a, b, c, d));
         } else if (TRIANGULAR_MEMBERSHIP_TYPE.contains(membershipType)) {
             if (split.length < 5) {
-                throw new LinguisticVariableParserException("Not enough arguments for Triangular membership function.");
+                throw new LinguisticVariableParserException("Linguistic value string " + linguisticValueString + "contains too few for Triangular membership function.");
             }
             double a = doubleParser.parse(split[2]);
             double b = doubleParser.parse(split[3]);
@@ -59,7 +59,7 @@ public class LinguisticValueParser {
             return new LinguisticValue(valueName, new TriangularMembershipFunction(a, b, c));
         } else if (GAUSSIAN_MEMBERSHIP_TYPE.contains(membershipType)) {
             if (split.length < 4) {
-                throw new LinguisticVariableParserException("Not enough arguments for Gaussian membership function.");
+                throw new LinguisticVariableParserException("Linguistic value string " + linguisticValueString + "contains too few for Gaussian membership function.");
             }
             double mean = doubleParser.parse(split[2]);
             double standardDeviation = doubleParser.parse(split[3]);
--- src/test/groovy/pl/polsl/linguisticVariable/parser/LinguisticValueParserSpec.groovy
+++ src/test/groovy/pl/polsl/linguisticVariable/parser/LinguisticValueParserSpec.groovy
@@ -1,8 +1,127 @@
 package pl.polsl.linguisticVariable.parser
 
+import pl.polsl.membershipFunction.GaussianMembershipFunction
+import pl.polsl.membershipFunction.TrapezoidalMembershipFunction
+import pl.polsl.membershipFunction.TriangularMembershipFunction
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class LinguisticValueParserSpec extends Specification {
-    //TODO
-    // todo split this class into multiple? Will see when writing tests.
+
+    NameParser nameParser = Stub()
+    DoubleParser doubleParser = Stub()
+
+    LinguisticValueParser linguisticValueParser = new LinguisticValueParser(nameParser, doubleParser)
+
+    def setup() {
+        nameParser.parse("High") >> "High"
+        doubleParser.parse("180") >> 180
+        doubleParser.parse("190") >> 190
+        doubleParser.parse("1000") >> 1000
+        doubleParser.parse("10") >> 10
+    }
+
+
+    @Unroll("should detect #expectedType membership function type for type input \"#inputType\"")
+    def "should detect membership function type"() {
+
+        given:
+        String input = String.format("High,%s,180,190,1000,1000", inputType)
+
+        when:
+        def result = linguisticValueParser.parse(input)
+
+        then:
+        expectedClass.isInstance(result.membershipFunction)
+
+        where:
+        inputType     | expectedType  | expectedClass
+        "T"           | "Trapezoidal" | TrapezoidalMembershipFunction.class
+        "t"           | "Trapezoidal" | TrapezoidalMembershipFunction.class
+        "Trap"        | "Trapezoidal" | TrapezoidalMembershipFunction.class
+        "Trapezoidal" | "Trapezoidal" | TrapezoidalMembershipFunction.class
+        "Tri"         | "Triangular"  | TriangularMembershipFunction.class
+        "tri"         | "Triangular"  | TriangularMembershipFunction.class
+        "Triangular"  | "Triangular"  | TriangularMembershipFunction.class
+        "G"           | "Gaussian"    | GaussianMembershipFunction.class
+        "g"           | "Gaussian"    | GaussianMembershipFunction.class
+        "Gauss"       | "Gaussian"    | GaussianMembershipFunction.class
+        "Gaussian"    | "Gaussian"    | GaussianMembershipFunction.class
+    }
+
+    def "should throw exception when membership function type is not recognized"() {
+        given:
+        String input = "High,X"
+
+        when:
+        linguisticValueParser.parse(input)
+
+        then:
+        thrown LinguisticVariableParserException
+    }
+
+    def "should create linguistic value with trapezoidal membership function"() {
+        given:
+        String input = "High,T,180,190,1000,1000"
+
+        when:
+        def result = linguisticValueParser.parse(input)
+
+        then:
+        result.name == "High"
+        result.membershipFunction instanceof TrapezoidalMembershipFunction
+        def membershipFunction = (TrapezoidalMembershipFunction) result.membershipFunction
+        membershipFunction.a == 180
+        membershipFunction.b == 190
+        membershipFunction.c == 1000
+        membershipFunction.d == 1000
+    }
+
+    def "should create linguistic value with triangular membership function"() {
+        given:
+        String input = "High,Tri,180,190,1000"
+
+        when:
+        def result = linguisticValueParser.parse(input)
+
+        then:
+        result.name == "High"
+        result.membershipFunction instanceof TriangularMembershipFunction
+        def membershipFunction = (TriangularMembershipFunction) result.membershipFunction
+        membershipFunction.a == 180
+        membershipFunction.b == 190
+        membershipFunction.c == 1000
+    }
+
+    def "should create linguistic value with gaussian membership function"() {
+        given:
+        String input = "High,G,180,10"
+
+        when:
+        def result = linguisticValueParser.parse(input)
+
+        then:
+        result.name == "High"
+        result.membershipFunction instanceof GaussianMembershipFunction
+        def membershipFunction = (GaussianMembershipFunction) result.membershipFunction
+        membershipFunction.mean == 180
+        membershipFunction.standardDeviation == 10
+    }
+
+
+    @Unroll("should throw exception when input \"#input\" #condition")
+    def "should throw exception when input contains too few arguments"() {
+        when:
+        linguisticValueParser.parse(input)
+
+        then:
+        thrown LinguisticVariableParserException
+
+        where:
+        input          | condition
+        "High,T"       | "contains too few arguments"
+        "High,T,1,2,3" | "contains too few arguments for trapezoidal membership function"
+        "High,Tri,1,2" | "contains too few arguments for triangular membership function"
+        "High,G,1"     | "contains too few arguments for gaussian membership function"
+    }
 }
